ffce7635c6da71279f39921a06c697bc77ef79d8
Joonhee <pakjoonhee90@gmail.com>
2018-10-28 22:01:02 -0700

    finish

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./Notes.css";
+import { s3Upload } from "../libs/awsLib";
 
 export default class Notes extends Component {
   constructor(props) {
@@ -62,7 +63,15 @@ export default class Notes extends Component {
     this.file = event.target.files[0];
   }
   
+  saveNote(note) {
+    return API.put("notes", `/notes/${this.props.match.params.id}`, {
+      body: note
+    });
+  }
+  
   handleSubmit = async event => {
+    let attachment;
+  
     event.preventDefault();
   
     if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
@@ -71,6 +80,25 @@ export default class Notes extends Component {
     }
   
     this.setState({ isLoading: true });
+  
+    try {
+      if (this.file) {
+        attachment = await s3Upload(this.file);
+      }
+  
+      await this.saveNote({
+        content: this.state.content,
+        attachment: attachment || this.state.note.attachment
+      });
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
+  }
+  
+  deleteNote() {
+    return API.del("notes", `/notes/${this.props.match.params.id}`);
   }
   
   handleDelete = async event => {
@@ -85,6 +113,14 @@ export default class Notes extends Component {
     }
   
     this.setState({ isDeleting: true });
+  
+    try {
+      await this.deleteNote();
+      this.props.history.push("/");
+    } catch (e) {
+      alert(e);
+      this.setState({ isDeleting: false });
+    }
   }
   
   render() {
