d163d795814df0174dcfb285794c1bc1c6d7acff
Joonhee <pakjoonhee90@gmail.com>
2018-10-28 21:33:39 -0700

    create notes

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,16 @@ import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    { /* Finally, catch all unmatched routes */ }
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
-    REGION: "us-east-1'",
-    BUCKET: "notes-app-uploads90"
+    REGION: "us-east-1",
+    BUCKET: "notes-apps-uploads90"
   },
   apiGateway: {
-    REGION: "us-east-1'",
+    REGION: "us-east-1",
     URL: "https://gz74hdfum5.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "us-east-1'",
+    REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_AciQYwrob",
     APP_CLIENT_ID: "44fa4lkngjfr316ifttn4a3v24",
     IDENTITY_POOL_ID: "us-east-1:6f67e116-abb0-4050-9741-176161548ac5"
