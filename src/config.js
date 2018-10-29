58d674ff2cecac4b2e68d4426cc3342c5605d455
Joonhee <pakjoonhee90@gmail.com>
2018-10-28 19:08:07 -0700

    about to add login page

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1'",
+    BUCKET: "notes-app-uploads90"
+  },
+  apiGateway: {
+    REGION: "us-east-1'",
+    URL: "https://gz74hdfum5.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1'",
+    USER_POOL_ID: "us-east-1_AciQYwrob",
+    APP_CLIENT_ID: "44fa4lkngjfr316ifttn4a3v24",
+    IDENTITY_POOL_ID: "us-east-1:6f67e116-abb0-4050-9741-176161548ac5"
+  }
+};
